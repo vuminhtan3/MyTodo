b70b1f9c1ff04f63e7b8473385fc212f85f44529
Minh Tan Vu <vuminhtan.nuce3@gmail.com>
2023-06-26 15:49:54 +0700

    Config .gitignore

--- Pods/Target Support Files/Pods-MyTodo-MyTodoUITests/Pods-MyTodo-MyTodoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyTodo_MyTodoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MyTodo_MyTodoUITests
-@end
--- Pods/Target Support Files/Pods-MyTodoTests/Pods-MyTodoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyTodoTests : NSObject
-@end
-@implementation PodsDummy_Pods_MyTodoTests
-@end
